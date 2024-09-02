d2742f7ec27cef2acc9f29ddc4c00a3cc2813e98
Dhruvsinghdk <re621006@gmail.com>
2024-09-02 20:04:19 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,13 @@
+a = {
+    "Name" : ("Roman","reings"),
+    "Age" : 26,
+    "Class" : 10,
+    "subject" : {
+        "php" : "Rajesh",
+        "Java" : "ram",
+        "Python" : "Ronit"
+    }
+};
+
+
+print(a["subject"])
